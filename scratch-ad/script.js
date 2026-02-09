36c6ce731746995718f90d8f1044c17c12b15368
01-AMURO <leess5731@gmail.com>
2026-02-09 13:20:12 +0900

    srcatch-ad

--- scratch-ad/script.js
+++ scratch-ad/script.js
@@ -5,7 +5,6 @@ const wrapper = document.querySelector('.scratch-wrapper');
 canvas.width = wrapper.clientWidth;
 canvas.height = wrapper.clientHeight;
 
-// 초기 덮개 설정
 ctx.fillStyle = '#3d3d47';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.fillStyle = '#888';
@@ -15,27 +14,23 @@ ctx.fillText('여기를 긁어보세요', canvas.width / 2, canvas.height / 2 + 
 
 let isDrawing = false;
 
-// 긁힌 비율을 계산하는 함수
 function checkScratchPercentage() {
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   const pixels = imageData.data;
   let transparentPixels = 0;
 
-  // 픽셀 배열은 [R, G, B, A] 순서로 저장됨 (A가 알파/투명도값)
   for (let i = 0; i < pixels.length; i += 4) {
-    if (pixels[i + 3] < 128) { // 투명도가 절반 이하로 떨어지면 긁힌 것으로 간주
+    if (pixels[i + 3] < 128) { 
       transparentPixels++;
     }
   }
 
   const percentage = (transparentPixels / (pixels.length / 4)) * 100;
 
-  // 40% 이상 긁혔을 때 실행
   if (percentage > 40) {
     canvas.style.transition = 'opacity 0.5s';
-    canvas.style.opacity = '0'; // 캔버스를 서서히 사라지게 함
+    canvas.style.opacity = '0'; 
     
-    // 잠시 후 링크 이동 (index.html에 정의된 clickTag 사용)
     setTimeout(() => {
       window.open(clickTag, '_blank');
     }, 1000);
@@ -54,7 +49,6 @@ function scratch(e) {
   ctx.arc(x, y, 20, 0, Math.PI * 2);
   ctx.fill();
 
-  // 긁을 때마다 비율 체크
   checkScratchPercentage();
 }
 
